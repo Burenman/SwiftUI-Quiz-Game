1b6c6273c928983d4aaec8aa8ef4c17a7c579c44
Viktor <Burenin777@gmail.com>
2023-11-11 17:30:44 -0500

    Initial Commit

---
+++ BizQuiz/BizQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  BizQuizApp.swift
+//  BizQuiz
+//
+//  Created by Viktor on 2023-11-11.
+//
+
+import SwiftUI
+
+@main
+struct BizQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
